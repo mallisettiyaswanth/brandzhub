627309f7faffe0a0d68eceb387e9ec5f19128701
Yaswanth Varada <yaswanthvarada20@gmail.com>
2025-01-04 20:46:46 +0530

    landing feature implemented

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,14 +2,14 @@ import Link from "next/link";
 
 export function Header() {
   return (
-    <header className="border-b fixed top-0 z-10 w-full bg-white ">
+    <header className="border border-gray-300 fixed top-0 z-10 w-full bg-transparent backdrop-blur rounded-xl  ">
       <div className="container mx-auto px-4 py-3">
         <div className="flex items-center justify-between gap-4 px-5 py-3">
           <Link href="/" className="text-xl font-bold">
             brandzhub
           </Link>
 
-          <nav className="flex items-center gap-8">
+          <nav className="flex items-center gap-8 font-bold ">
             <Link href="/" className="text-sm">
               Home
             </Link>
