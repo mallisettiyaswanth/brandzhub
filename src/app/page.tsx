59106ec204f2f20cd097df3d7c88cdedf97b8cf4
Yaswanth Varada <yaswanthvarada20@gmail.com>
2025-04-13 11:42:12 +0530

    temporary release on request

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,31 +8,31 @@ import { CarouselPlugin } from "@/components/carousel-plugin";
 import FeaturesSection from "@/components/features-section";
 import PaymentWarning from "@/components/payment-warning";
 
-// export default function Home() {
-//   return (
-//     <>
-//       <Header />
-//       <div className="flex min-h-screen flex-col bg-gray-50 pt-20">
-//         <main className="flex-grow">
-//           <CarouselPlugin />
-//           <CategoryGrid />
-//           <Hero />
-//           {/* <div className="container mx-auto"> */}
-//           <SeasonalBanner />
-//           <ProductGrid />
-//           <FeaturesSection />
-//           {/* </div> */}
-//         </main>
-//         <Footer />
-//       </div>
-//     </>
-//   );
-// }
-
 export default function Home() {
   return (
     <>
-      <PaymentWarning />
+      <Header />
+      <div className="flex min-h-screen flex-col bg-gray-50 pt-20">
+        <main className="flex-grow">
+          <CarouselPlugin />
+          <CategoryGrid />
+          <Hero />
+          {/* <div className="container mx-auto"> */}
+          <SeasonalBanner />
+          <ProductGrid />
+          <FeaturesSection />
+          {/* </div> */}
+        </main>
+        <Footer />
+      </div>
     </>
   );
 }
+
+// export default function Home() {
+//   return (
+//     <>
+//       <PaymentWarning />
+//     </>
+//   );
+// }
